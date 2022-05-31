34b0fa0e9fd6d389c81d45a2c74d6fd02dbdd235
Pedro Serejo <serejoph@me.com>
2022-05-30 22:19:15 -0300

    CrossOrigin

--- src/main/java/pedro/serejo/TimestampMicroservice/controller/TimestampController.java
+++ src/main/java/pedro/serejo/TimestampMicroservice/controller/TimestampController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import pedro.serejo.TimestampMicroservice.service.DateService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class TimestampController {
