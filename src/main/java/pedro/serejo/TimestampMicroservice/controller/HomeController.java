6acd21282cd29a5b4472da902d93c1fb7e4437cd
Pedro Serejo <serejoph@me.com>
2022-05-30 21:28:09 -0300

    Request Mapping

--- src/main/java/pedro/serejo/TimestampMicroservice/controller/HomeController.java
+++ src/main/java/pedro/serejo/TimestampMicroservice/controller/HomeController.java
@@ -1,11 +1,15 @@
 package pedro.serejo.TimestampMicroservice.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("")
+@RestController
+@RequestMapping("")
 public class HomeController {
 
+	@GetMapping
 	public ResponseEntity<String> home(){
 		return ResponseEntity.ok("Home");
 	}
