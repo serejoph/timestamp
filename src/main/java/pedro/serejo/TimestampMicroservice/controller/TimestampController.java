27e49ff2244a48582aaa2de88d9dbd9fd1ecb73e
Pedro Serejo <serejoph@me.com>
2022-05-30 21:11:33 -0300

    Content Type

--- src/main/java/pedro/serejo/TimestampMicroservice/controller/TimestampController.java
+++ src/main/java/pedro/serejo/TimestampMicroservice/controller/TimestampController.java
@@ -2,6 +2,7 @@ package pedro.serejo.TimestampMicroservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,14 +22,14 @@ public class TimestampController {
 	@GetMapping("/{date}")
 	@ResponseBody
 	public ResponseEntity<String> getTimestamp(@PathVariable(name = "date") String date) {
-		return ResponseEntity.status(HttpStatus.OK).body(dateService.convert(date));
+		return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(dateService.convert(date));
 		
 
 	}
 
 	@GetMapping()
 	public ResponseEntity<String> currentTimestamp() {
-		return ResponseEntity.status(HttpStatus.OK).body(dateService.now());
+		return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(dateService.now());
 		
 	}
 }
